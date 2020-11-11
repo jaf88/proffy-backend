1ac5629cc36555bdb8cd385f85d6b905e5f55ad0
jaf88 <jaf.alves8@gmail.com>
2020-11-11 10:49:22 -0300

    chore: database configuration

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import express from 'express';
+
+const app = express();
+
+app.get('/express', (request, response) => {
+  const users = [
+    "Joaquim"
+  ];
+  return response.json(users);
+})
+
+app.listen(3333);
