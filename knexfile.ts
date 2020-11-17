0e0b9b4659c425fad0e209bae1117eb3d2ef073e
jaf88 <jaf.alves8@gmail.com>
2020-11-17 02:15:02 -0300

    feat: config deploy on heruku

--- knexfile.ts
+++ knexfile.ts
@@ -10,6 +10,14 @@ module.exports = {
    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
  },
 
+ production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+  },
+},
+
  useNullAsDefault: true,
 
 };
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
@@ -9,4 +12,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
